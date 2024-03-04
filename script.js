8181429fffd77cb07fb0c040b046f82c399a1785
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-03-05 00:01:55 +0600

    added text animation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Shery.imageEffect("#back", {
     a: { value: 0.92, range: [0, 30] },
     b: { value: -0.95, range: [-1, 1] },
     zindex: { value: -9996999, range: [-9999999, 9999999] },
-    aspect: { value: 1.904109589041096 },
+    aspect: { value: 1.5374077976817704 },
     ignoreShapeAspect: { value: true },
     shapePosition: { value: { x: 0, y: 0 } },
     shapeScale: { value: { x: 0.5, y: 0.5 } },
@@ -23,14 +23,50 @@ Shery.imageEffect("#back", {
     scrollType: { value: 0 },
     geoVertex: { range: [1, 64], value: 1 },
     noEffectGooey: { value: true },
-    onMouse: { value: 1 },
-    noise_speed: { value: 0.61, range: [0, 10] },
-    metaball: { value: 0.14, range: [0, 2], _gsap: { id: 3 } },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.99, range: [0, 10] },
+    metaball: { value: 0.17, range: [0, 2], _gsap: { id: 3 } },
     discard_threshold: { value: 0.53, range: [0, 1] },
     antialias_threshold: { value: 0, range: [0, 0.1] },
     noise_height: { value: 0.5, range: [0, 2] },
-    noise_scale: { value: 10, range: [0, 100] },
+    noise_scale: { value: 11.45, range: [0, 100] },
   },
   debug: false,
   gooey: true,
 });
+
+
+
+
+var elems = document.querySelectorAll('.elem');
+
+elems.forEach((elem) => {
+    var h1s = elem.querySelectorAll('h1');
+    var index = 0;
+
+    var animating = false;
+
+    document.getElementById("main").addEventListener('click', function(){
+        if(!animating){
+            animating = true;
+            gsap.to(h1s[index], {
+                top: '-=100%',
+                ease: Expo.easeInOut,
+                duration: 1,
+                onComplete: function(){
+                    gsap.set(this._targets[0], {top: '100%'});
+                    animating = false;
+                }
+            })
+    
+            index === h1s.length-1? (index = 0) : index++;
+    
+            gsap.to(h1s[index], {
+                top: '-=100%',
+                ease: Expo.easeInOut,
+                duration: 1,
+            });
+        }
+    });
+})
+
